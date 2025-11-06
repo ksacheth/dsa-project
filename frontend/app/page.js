aace3b39cab7854de16cf47bc105a22ad8cac470
Sacheth Koushal <punnasachethkoushal@gmail.com>
2025-11-06 10:46:00 +0530

    initial commit

---
+++ frontend/app/page.js
@@ -0,0 +1,14 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      <div className="text-6xl font-bold">
+          URL Shortner
+      </div>
+      <div>
+        
+      </div>
+    </div>
+  );
+}
